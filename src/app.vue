451d4c7aa385f694ff828a2adcdd4701e4c1269d
aleago02 <alessandro.agostin13@studio.unibo.it>
2022-12-29 14:10:49 +0100

    aggiunto la funzione di searc

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,33 @@
+<script lang="ts">
+
+export default defineComponent({
+  data() {
+    return {
+      searchGame: "",
+    }
+  },
+  methods: {
+    search(){
+      $fetch("/api/search/search", {
+        method: "POST",
+        body: {
+          game: this.searchGame,
+        }
+      })
+        .then((response) => window.location.href = "/game/" + response)
+        .catch((e) => alert(e))
+    },
+  }
+})
+</script>
+
+
 <template>
   <header>
     <h1><a href="/">Game Shop</a></h1>
     <form class="modulo-ricerca">
-      <input id="search" type="text" placeholder="Cerca nel sito" required>
-      <input id="submit" type="submit" value="CERCA">
+      <input id="search" type="text" placeholder="Cerca nel sito" v-model="searchGame">
+      <input id="submit" type="button" value="cerca" @click=search()>
     </form>
     <a href="/api/login"><img class="login" src="/img/login.png" /></a>
     <a href="/api/shop"><img class="shop" src="/img/shop.png" /></a>
---
+++ src/server/api/search/search.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcrypt"
+import { createConnection } from "~/server/utils/db"
+
+
+export default defineEventHandler(async function(event) {
+
+    const { game } = await readBody(event)
+  
+    // Esegue la query al database per ottenere i dati dell'utente in base allo username
+    const connection = await createConnection()
+    const [results] = await connection.execute(
+      `SELECT idgame, gamename, platform, relasedate, price, rate, img, namekind
+      FROM game INNER JOIN  image
+      ON game.idgame = image.game
+      INNER JOIN kind_of_game
+      ON kind_of_game.game = game.idgame
+      INNER JOIN kind
+      ON kind.idkind = kind_of_game.kind;`,
+      [game]
+    )
+  
+    // Errore se l'utente non è stato trovato
+    if (!Array.isArray(results) || results.length == 0) {
+      throw createError({ statusCode: 400, statusMessage: "Nome errate"})
+    }
+  
+  
+    // Confronta l'hash della password fornita con quello nel database
+    let i = 0
+  
+    for (const name in results){
+        i = i + 1
+        if (game == results[i].gamename){
+            return  String(results[i].idgame)
+        }
+    }
+
+    
+    throw createError({ statusCode: 400, statusMessage: "Nome errato"})
+   
+    
+  
+})
