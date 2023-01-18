4ae7f5369c6f7627e58fb55b1a7e33501901b459
Lorenzofini <lorenzo.fini2@studio.unibo.it>
2023-01-18 16:14:29 +0100

    cart without totale

--- src/pages/cart.vue
+++ src/pages/cart.vue
@@ -29,24 +29,16 @@ export default defineComponent({
   <section>
       <h2>CARRELLO</h2>
       <section v-for="game in games">
-<<<<<<< HEAD
-        <h3>{{game.gamename}}</h3><p>{{game.platform}}</p>
-        <img :src="'/img/' + game.img" alt="" />
-        <p>{{game.price}}€</p>
-        <p>Quantità:{{ game.quantità }}</p>
-      </section>
-    </div>
-=======
         <img :src="'/img/' + game.img" alt="" />
         <div>
           <h3>{{game.gamename}}</h3><p>{{game.platform}}</p>
         </div>
         <div id="price">
           <p>{{game.price}}€</p>
+          <p>Quantità:{{ game.quantità }}</p>
         </div>
         <hr>
       </section>
->>>>>>> a7d13b44461491d4c636e45d060804322d4a59de
   </section>
   <section>
     <h3>Totale €</h3>
